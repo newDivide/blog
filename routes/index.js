9c1c441044ab59b28a7ad7b5b6db5f67462b81f9
hzgegenhasi <hzgegenhasi@corp.netease.com>
2015-10-12 16:05:04 +0800

    路由配置

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 module.exports = function(app) {
-  app.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+ app.get('/', function (req, res) {
+    res.render('index');
+  });
+  app.get('/article', function (req, res) {
+    res.render('article');
+  });
+  app.get('/timeline', function (req, res) {
+    res.render('timeline');
+  });
+  app.get('/messageBoard', function (req, res) {
+    res.render('messageBoard');
+  });
+  app.get('/tag', function (req, res) {
+    res.render('tag');
   });
 };
